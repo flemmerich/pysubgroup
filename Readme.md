f143c2c402e1c44c140f41251639d546e8425547
lemmerich <florian_lemmerich>
2018-05-11 08:27:38 +0200

    Added setup.py + Readme.md

---
+++ Readme.md
@@ -0,0 +1,4 @@
+**Pysubgroup**
+
+**pysubgroup** is a Python package that enables subgroup discovery in Python+pandas (scipy stack) data analysis environment. It provides for a lightweight, easy-to-use, extensible and freely available implementation of state-of-the-art algorithms, interestingness measures and presentation options.
+
--- pysubgroup.tests/test_titanic.py
+++ pysubgroup.tests/test_titanic.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 data = pd.read_csv("~/datasets/titanic.csv")
-target = ps.NominalTarget ('survived', 0)
+target = ps.NominalTarget ('survived', True)
 searchSpace = ps.createSelectors(data, ignore=['survived'])
 task = ps.SubgroupDiscoveryTask (data, target, searchSpace,
                                  resultSetSize=5, depth=2,
                                  qf=ps.StandardQF(1))
 
-result = ps.Apriori().execute(task)
+result = ps.BeamSearch().execute(task)
 
 for (q, sg) in result:
     print (str(q) + ":\t" + str(sg.subgroupDescription))
