9ce0f58c9c44d42ec1be075593c63a545f8e2bed
fs385590 <felix.stamm@rwth-aachen.de>
2019-10-02 13:44:38 +0200

    Added pytest folders to gitignore, updated simple dfs

--- pysubgroup/tests/t_simple_dfs.py
+++ pysubgroup/tests/t_simple_dfs.py
@@ -3,9 +3,10 @@ from scipy.io import arff
 import pysubgroup as ps
 import pandas as pd
 from timeit import default_timer as timer
+from pysubgroup.tests.DataSets import getCreditData
+data = getCreditData()
 
-data = pd.DataFrame(arff.loadarff("../data/credit-g.arff")[0])
-
+print("running")
 target = ps.NominalTarget('class', b'bad')
 search_space = ps.create_selectors(data, ignore=['class'])
 task = ps.SubgroupDiscoveryTask(data, target, search_space, result_set_size=10, depth=3, qf=ps.ChiSquaredQF(direction="bidirect"))
