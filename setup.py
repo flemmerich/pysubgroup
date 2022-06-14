24f530f5012178e486bb3a812c6c9ab4bf229901
mgbckr <1241516+mgbckr@users.noreply.github.com>
2022-06-14 14:21:32 -0700

    Prepare hotfix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='pysubgroup',
-    version='0.7.2',
+    version='0.7.3',
     packages=['pysubgroup', 'pysubgroup.tests'],
     package_data={'pysubgroup': ['data/credit-g.arff', 'data/titanic.csv']},
     url='http://florian.lemmerich.net/pysubgroup',
     license='',
-    author='Florian Lemmerich, Felix Stamm',
+    author='Florian Lemmerich, Martin Becker, Felix Stamm',
     author_email='florian@lemmerich.net',
     description='pysubgroup is a Python library for the data analysis task of subgroup discovery.',
     install_requires=[
